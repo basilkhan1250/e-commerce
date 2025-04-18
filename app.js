32b42729bd9f1e862b0be82ea17c49f2970be6f0
M.BasilKhan <basilkhan1250@gmail.com>
2025-04-19 03:28:19 +0500

    workin on it

--- app.js
+++ app.js
@@ -1,63 +1,63 @@
 // localStorage.clear(); 
 
 
-let carsData = [
-    {
-        name: 'Lamborghini',
-        model: "Urus",
-        color: 'Orange',
-        price: 300000,
-        imgSrc: "https://hips.hearstapps.com/hmg-prod/images/2025-lamborghini-urus-se-phev-106-67005496322ba.jpg?crop=0.633xw:0.534xh;0.223xw,0.427xh&resize=1200:*",
-        isImported: true
-    },
-    {
-        name: 'Mehran',
-        model: "VX",
-        color: 'Silver',
-        price: 2500,
-        imgSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrJtBX7b_ukY1rNcPgP_Y7BzG2AwWJecct6Q&s",
-        isImported: false
-    },
-    {
-        name: 'Sonata',
-        model: "N Line",
-        color: 'White',
-        price: 60000,
-        imgSrc: "https://propakistani.pk/wp-content/uploads/2025/01/N-Line.jpg",
-        isImported: true
-    },
-    {
-        name: 'Havel',
-        model: "H6",
-        color: 'black',
-        price: 40000,
-        imgSrc: "https://i.ytimg.com/vi/wAC15UwhCXo/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBUbNTquKFO2W1t_bhbheKPMdYgrg",
-        isImported: false
-    }
-];
+// let carsData = [
+//     {
+//         name: 'Lamborghini',
+//         model: "Urus",
+//         color: 'Orange',
+//         price: 300000,
+//         imgSrc: "https://hips.hearstapps.com/hmg-prod/images/2025-lamborghini-urus-se-phev-106-67005496322ba.jpg?crop=0.633xw:0.534xh;0.223xw,0.427xh&resize=1200:*",
+//         isImported: true
+//     },
+//     {
+//         name: 'Mehran',
+//         model: "VX",
+//         color: 'Silver',
+//         price: 2500,
+//         imgSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrJtBX7b_ukY1rNcPgP_Y7BzG2AwWJecct6Q&s",
+//         isImported: false
+//     },
+//     {
+//         name: 'Sonata',
+//         model: "N Line",
+//         color: 'White',
+//         price: 60000,
+//         imgSrc: "https://propakistani.pk/wp-content/uploads/2025/01/N-Line.jpg",
+//         isImported: true
+//     },
+//     {
+//         name: 'Havel',
+//         model: "H6",
+//         color: 'black',
+//         price: 40000,
+//         imgSrc: "https://i.ytimg.com/vi/wAC15UwhCXo/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBUbNTquKFO2W1t_bhbheKPMdYgrg",
+//         isImported: false
+//     }
+// ];
 
 
-let carListEl = document.querySelectorAll(".cars-list-container")[0];
+// let carListEl = document.querySelectorAll(".cars-list-container")[0];
 
-function listCars(cars) {
-    for (let i = 0; i < cars.length; i++) {
-        let carCard = makeCarCard(cars[i]);
-        carListEl.innerHTML += carCard;
-    }
-}
+// function listCars(cars) {
+//     for (let i = 0; i < cars.length; i++) {
+//         let carCard = makeCarCard(cars[i]);
+//         carListEl.innerHTML += carCard;
+//     }
+// }
 
-listCars(carsData);
+// listCars(carsData);
 
 
-function makeCarCard(car) {
-    return `<div>
-                <img src="${car.imgSrc}" alt="${car.name}"/>
-                <h2>${car.name} ${car.model}</h2>
-                <p>${car.color}</p>
-                <p>${car.price}</p>
-            </div>
-        `
-}
+// function makeCarCard(car) {
+//     return `<div>
+//                 <img src="${car.imgSrc}" alt="${car.name}"/>
+//                 <h2>${car.name} ${car.model}</h2>
+//                 <p>${car.color}</p>
+//                 <p>${car.price}</p>
+//             </div>
+//         `
+// }
 
 
 
@@ -65,20 +65,37 @@ function makeCarCard(car) {
 function signUp(event) {
     event.preventDefault();
 
-    const signupUserName = document.querySelector(".username").value;
-    const signupUserPassword = document.querySelector(".password").value;
-    const signupUserEmail = document.querySelector(".email").value;
+    const signupUserName = document.querySelector(".username")
+    const signupUserPassword = document.querySelector(".password")
+    const signupUserEmail = document.querySelector(".email")
+    let error = document.querySelectorAll("p");
+    console.log(error)
 
     let users = JSON.parse(localStorage.getItem("users")) || [];
-
+    console.log(users)
     const userExist = users.some(user =>
-        user.userName === signupUserName || user.userEmail === signupUserEmail
+        user.userName === signupUserName.value || user.userEmail === signupUserEmail.value
+        
     );
 
     if (userExist) {
-        alert("User already exists");
+
+        setInterval(() => {
+            error[0].style.display = "none";
+        }, 3000);
+        signupUserEmail.value = ""
+        signupUserName.value = ""
+        signupUserPassword.value = ""
+        error[0].innerHTML = "User already exists";
+        error[0].style.display = "block";
+        error[0].style.color = "red";
+        error[0].style.fontSize = "20px";
+        // alert("User already exists");
         return;
     }
+
+
+
 
     const newUser = {
         userName: signupUserName,
@@ -90,26 +107,54 @@ function signUp(event) {
 
     localStorage.setItem("users", JSON.stringify(users));
 
-    alert("User Created Successfully");
-    window.location.href = "./login.html";
+    error[0].innerHTML = "User Created Successfully";
+    error[0].style.display = "block";
+    error[0].style.color = "green";
+    error[0].style.fontSize = "20px";
+    signupUserEmail.value = ""
+    signupUserName.value = ""
+    signupUserPassword.value = ""
+    // alert("User Created Successfully")
+    setInterval(() => {
+        window.location.href = "./login.html";
+    }, 2000);
 }
 
 
 function login(event) {
     event.preventDefault();
 
-    const loginUserName = document.querySelector(".username").value;
-    const loginUserPassword = document.querySelector(".password").value;
+    const error = document.querySelectorAll("p")
+    console.log(error)
+    const loginUserName = document.querySelector(".username");
+    const loginUserPassword = document.querySelector(".password");
     console.log(loginUserName, loginUserPassword);
 
     let users = JSON.parse(localStorage.getItem("users")) || [];
-    const validUser = users.find(user => (user.userName === loginUserName && user.userPassword === loginUserPassword))
+    const validUser = users.find(user => (user.userName === loginUserName.value && user.userPassword === loginUserPassword.value))
     if (validUser) {
-        alert("login successfully")
-        window.location.href = "./products.html";
+        error[0].innerHTML = "Login Successfully";
+        error[0].style.display = "block";
+        error[0].style.color = "green";
+        error[0].style.fontSize = "20px";
+        loginUserName.value = "";
+        loginUserPassword.value = "";
+        setInterval(() => {
+            window.location.href = "./products.html";
+        }, 2000);
     }
     else {
-        alert("Invalid username or password")
+        error[0].innerHTML = "Invalid username or password";
+        error[0].style.display = "block";
+        error[0].style.color = "red";
+        error[0].style.fontSize = "20px";
+
+        loginUserName.value = "";
+        loginUserPassword.value = "";
+
+        setInterval(() => {
+            error[0].style.display = "none";
+        }, 3000);
     }
 
 }
